210aa8f2211b361ab3265eb39727eec9ee0acc7f
arturo <arturomazzoni@gmail.com>
2021-01-28 17:48:01 +0100

    list

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var app = new Vue ({
+  el: "#app",
+  data: {
+    list: [
+      "Season one",
+      "Season two",
+      "Season three",
+      "Season four",
+      "Season five"
+    ]
+  }
+});
