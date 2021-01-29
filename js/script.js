68dbc93ca88b66a92a8be8735b539e74eccb9398
arturo <arturomazzoni@gmail.com>
2021-01-29 14:41:22 +0100

    delete/remove

--- js/script.js
+++ js/script.js
@@ -9,18 +9,29 @@ var app = new Vue ({
       "Season five"
     ],
     listDeleted: [],
+    newItem: ""
   },
   methods: {
-    deleteItem(index){
+    removeItem(index){
       this.listDeleted.push(this.list[index]);
       // alert("You've watched: " + this.list[index]);
       this.list.splice(index,1);
-      console.log(this.listDeleted);
+    },
+    deleteItem(index){
+      this.listDeleted.splice(index,1);
     },
     restoreItem(index){
       this.list.push(this.listDeleted[index]);
       // alert("You're restoring: " + this.list[index]);
       this.listDeleted.splice(index,1);
+    },
+    addItem(){
+      if (this.newItem.length > 3){
+        this.list.push(this.newItem);
+        this.newItem = "";
+      } else {
+        alert("Type at least 4 characters");
+      }
     }
   }
 });
